0dbc12bfb979f1a30e2a929de30542b340d8e96d
oliverhaugh <121741395+oliverhaugh@users.noreply.github.com>
2023-01-03 15:46:47 -0500

    Add files via upload

---
+++ program that prints out the date.py
@@ -0,0 +1,108 @@
+# This is a Program by Oliver H
+# I hope You Enjoy
+
+
+# These are the variables we check in the if and elif statements
+month = int(input("What is the month: "))
+day = int(input("What is the day: "))
+year = int(input("What is the year: "))
+date = None
+# Month
+if month == 1:
+    month = "January"
+elif month == 2:
+    month = "February"
+elif month == 3:
+    month = "March"
+elif month == 4:
+    month = "April"
+elif month == 5:
+    month = "May"
+elif month == 6:
+    month = "June"
+elif month == 7:
+    month = "July"
+elif month == 8:
+    month = "August"
+elif month == 9:
+    month = "September"
+elif month == 10:
+    month = "October"
+elif month == 11:
+    month = "November"
+elif month == 12:
+    month = "December"
+else:
+    month = "Unknown Date"
+
+# day
+if day == 1:
+    day = 1
+elif day == 2:
+    day = 2
+elif day == 3:
+    day = 3
+elif day == 4:
+    day = 4
+elif day == 5:
+    day = 5
+elif day == 6:
+    day = 6
+elif day == 7:
+    day = 7
+elif day == 8:
+    day = 8
+elif day == 9:
+    day = 9
+elif day == 10:
+    day = 10
+elif day == 11:
+    day = 11
+elif day == 12:
+    day = 12
+elif day == 13:
+    day = 13
+elif day == 14:
+    day = 14
+elif day == 15:
+    day = 15
+elif day == 16:
+    day = 16
+elif day == 17:
+    day = 17
+elif day == 18:
+    day = 18
+elif day == 18:
+    day = 18
+elif day == 19:
+    day = 19
+elif day == 20:
+    day = 20
+elif day == 21:
+    day = 21
+elif day == 22:
+    day = 22
+elif day == 23:
+    day = 23
+elif day == 24:
+    day = 24
+elif day == 25:
+    day = 25
+elif day == 26:
+    day = 26
+elif day == 27:
+    day = 27
+elif day == 28:
+    day = 28
+elif day == 29:
+    day = 29
+elif day == 30:
+    day = 30
+elif day == 31:
+    day = 31
+else:
+    day = "Unknown day"
+
+# printing the date
+print("The date is ", " ", month, " ", day, ", ", year)
+    
