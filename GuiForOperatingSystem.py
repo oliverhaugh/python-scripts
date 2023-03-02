6355e918bcd94392056ad61f5e65908bc8801972
oliverhaugh <121741395+oliverhaugh@users.noreply.github.com>
2023-03-02 16:06:14 -0500

    Add files via upload

---
+++ GuiForOperatingSystem.py
@@ -0,0 +1,40 @@
+from tkinter import *
+
+root = Tk()
+root.title("My Operating System")
+
+# Create menu bar
+menubar = Menu(root)
+filemenu = Menu(menubar, tearoff=0)
+filemenu.add_command(label="New")
+filemenu.add_command(label="Open")
+filemenu.add_command(label="Save")
+filemenu.add_command(label="Save As...")
+filemenu.add_separator()
+filemenu.add_command(label="Exit", command=root.quit)
+menubar.add_cascade(label="File", menu=filemenu)
+
+editmenu = Menu(menubar, tearoff=0)
+editmenu.add_command(label="Undo")
+editmenu.add_separator()
+editmenu.add_command(label="Cut")
+editmenu.add_command(label="Copy")
+editmenu.add_command(label="Paste")
+editmenu.add_command(label="Delete")
+editmenu.add_command(label="Select All")
+menubar.add_cascade(label="Edit", menu=editmenu)
+
+helpmenu = Menu(menubar, tearoff=0)
+helpmenu.add_command(label="Help Index")
+helpmenu.add_command(label="About...")
+menubar.add_cascade(label="Help", menu=helpmenu)
+
+# Display menu bar
+root.config(menu=menubar)
+
+# Create a label
+label = Label(root, text="Welcome to My Operating System!")
+label.pack()
+
+# Run the main loop
+root.mainloop()
